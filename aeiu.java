e9f44dded7502aa9f6128eae97cc9d841733e16d
silpaaswathysuresh <43902252+silpaaswathysuresh@users.noreply.github.com>
2018-10-06 14:18:38 +0530

    Update aeiu.java

--- aeiu.java
+++ aeiu.java
@@ -4,12 +4,12 @@ public class Main
 {
   public static void main(String[]args)
   {
-
+int i=0;
 Scanner sc=new Scanner(System.in);
 char ch=sc.next().charAt(0);
-if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
 System.out.println("vowel");
-else if(ch>='a'&& ch<='z'||ch>='A'&& ch<='z')
+else if(ch>='a'&& ch<='z')
 System.out.println("consonant");
 else
 System.out.println("invalid");

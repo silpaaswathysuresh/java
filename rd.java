300117582de42d6387409a59bd129b02f0181030
silpaaswathysuresh <43902252+silpaaswathysuresh@users.noreply.github.com>
2018-10-06 13:42:35 +0530

    Update rd.java

--- rd.java
+++ rd.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 class Main 
 {
   public static void main(String[]args){
-  int num;
+  int a;
   Scanner rd=new Scanner(System.in);
       num=rd.nextInt();
  if(num%2==0)
-  System.out.println(num+"is even");
+  System.out.println(a+"is even");
   else
- System.out.println(num +"is odd");
+ System.out.println(a +"is odd");
   }
  }

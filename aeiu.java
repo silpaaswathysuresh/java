f92ddc33e933e79834b4867380b666a31439ac2b
silpaaswathysuresh <43902252+silpaaswathysuresh@users.noreply.github.com>
2018-10-06 14:16:36 +0530

    Update aeiu.java

--- aeiu.java
+++ aeiu.java
@@ -4,7 +4,7 @@ public class Main
 {
   public static void main(String[]args)
   {
-int i=0;
+
 Scanner sc=new Scanner(System.in);
 char ch=sc.next().charAt(0);
 if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')

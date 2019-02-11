af687f42b7a1bcf44c4d7baabd99e2fb6948c0b7
silpaaswathysuresh <43902252+silpaaswathysuresh@users.noreply.github.com>
2019-02-11 15:20:00 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,27 @@
+// Welcome to SpiderLabWeb
 import java.util.*;
-import java.util.Scanner;
-class Main 
-{
-  public static void main(String[]args){
-  int s;
-  Scanner sc=new Scanner(System.in);
-   num=sc.nextInt();
-   if(num>0)
-   System.out.println(s +"  positive");
-   else if(num<0)
-   System.out.println(s+"  negative");
-   else
-   System.out.println("it is zero");
-}
-}
+
+class Main {
+  
+  public static void main(String[] args) {
+int a,b,c;
+Scanner s =new Scanner(System.in);
+System.out.println("enter the first number:");
+a=s.nextInt();
+System.out.println("enter the second number:");
+b=s.nextInt();
+System.out.println("enter the third number:");
+c=s.nextInt();
+  if(a<b && b<c)
+
+  System.out.println("first number is large");
+else if(a<b && a>c)
+
+System.out.println("second number is large");
+
+ else if(a>b && a<c) 
+  
+ System.out.println( "third number is large");
+  
+  
+} }

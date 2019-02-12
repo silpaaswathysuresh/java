038c8fad5cd6a19209c8d1622f8ddb880500472a
silpaaswathysuresh <43902252+silpaaswathysuresh@users.noreply.github.com>
2019-02-12 12:21:01 +0530

    Add files via upload

---
+++ nn.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    int i,n;
+    char hello;
+    Scanner qwe=new Scanner(System.in);
+     n=qwe.nextInt();
+   
+    for(i=0;i<=n;i++)
+    {
+    System.out.println("Hello");
+  }
+}}
